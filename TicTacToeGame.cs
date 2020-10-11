54e3d84dd4bb0b274823adc9d3013533a1f3ca71
Akash Pratap Singh <akashpratap2507@gmail.com>
2020-10-11 20:46:19 +0530

    [Akash] Added Create Board Program

---
+++ TicTacToeGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class TicTacToeGame
+    {
+        public static char[] CreateBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 0; i < board.Length; i++)
+            { 
+                board[i] = ' '; 
+            }
+            return board;
+        }
+        
+    }
+}
